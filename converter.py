9559e2aacac8ad1371d02a2af99c898f1a99ed63
Alexander Steen <alexander.steen@qapital.com>
2023-12-21 18:05:47 +0100

    Converter

--- converter.py
+++ converter.py
@@ -2,10 +2,21 @@ import sys
 print(sys.argv[0])
 input = sys.argv[1]
 target = sys.argv[2]
-print(input)
 convertFrom = input[0]
 convertTo = input[1]
 
-print(convertFrom)
-print(convertTo)
-print(target)
+tempValue = 0
+if convertFrom == 'h':
+    tempValue = int(target, 16)
+
+elif convertFrom == 'b':
+    tempValue = int(target,2)
+else:
+    tempValue = int(target)
+
+if convertTo == 'b':
+    print(format(tempValue, "b"))
+elif convertTo == 'h':
+    print(hex(tempValue))
+else:
+    print(tempValue)
