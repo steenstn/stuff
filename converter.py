51a895bffde5853e69830e3e3eb05f6fec9a7ff2
Alexander Steen <alexander.steen@qapital.com>
2023-12-21 17:56:47 +0100

    Added start of converter

---
+++ converter.py
@@ -0,0 +1,11 @@
+import sys
+print(sys.argv[0])
+input = sys.argv[1]
+target = sys.argv[2]
+print(input)
+convertFrom = input[0]
+convertTo = input[1]
+
+print(convertFrom)
+print(convertTo)
+print(target)
