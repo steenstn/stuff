d0284b296c1c1c91c08783bc6c864757aa38935d
Alexander Steen <alexander.steen@qapital.com>
2023-12-26 22:01:02 +0100

    Added info

--- ascii.py
+++ ascii.py
@@ -1,4 +1,15 @@
 import sys
+
+def printInfo():
+    print("""
+Convert from to ASCII
+Usage: python3 ascii.py 65
+       python3 ascii.py A
+    """)
+
+if len(sys.argv) == 1:
+    printInfo()
+    quit()
 if sys.argv[1].isdigit():
     print(chr(int(sys.argv[1])))
 else:
