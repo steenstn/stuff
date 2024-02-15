89e674b5690787e516844d3cb5638159b3628677
Alexander Steen <alexander.steen@qapital.com>
2024-02-15 15:50:54 +0100

    COmment

--- join.sh
+++ join.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Uses ical-buddy to find the next upcoming event in the calendar and opens the google meet link contained in the description
+# Uses ical-buddy to find the next upcoming event in the calendar and opens the meeting link contained in the description
+# Searches for a Google/Zoom link and opens it
 
 meeting_title=$(icalBuddy -n -li 1 -b "" -iep title eventsToday)
-meeting_link=$(icalBuddy -n -li 1 eventsToday | grep -o "https://meet.google.com/\w.*-\w.*-\w.")
+meeting_link=$(icalBuddy -n -li 1 eventsToday | grep -o "https://meet.google.com/\w.*-\w.*-\w." | head -1)
 
 if [[ -z "${meeting_link}" ]]; then
   meeting_link=$(icalBuddy -n -li 1 eventsToday | grep -o "https://wedbush.zoom.us/[^\s]/\d*" | head -1)
