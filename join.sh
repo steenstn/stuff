7c94aa455f3f93d37b31d4330ea1074ce85088c9
Alexander Steen <alexander.steen@qapital.com>
2024-02-15 15:41:24 +0100

    Added support for zoom meetings

--- join.sh
+++ join.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 # Uses ical-buddy to find the next upcoming event in the calendar and opens the google meet link contained in the description
 
-open $(icalBuddy -n -li 1 eventsToday | grep "https://meet.google.com/\w.*-\w.*-\w.*" | sed 's/^.*https/https/')
+meeting_title=$(icalBuddy -n -li 1 -b "" -iep title eventsToday)
+meeting_link=$(icalBuddy -n -li 1 eventsToday | grep -o "https://meet.google.com/\w.*-\w.*-\w.")
+
+if [[ -z "${meeting_link}" ]]; then
+  meeting_link=$(icalBuddy -n -li 1 eventsToday | grep -o "https://wedbush.zoom.us/[^\s]/\d*" | head -1)
+fi
+
+echo "Joining ${meeting_title} at ${meeting_link}"
+
+open ${meeting_link}
