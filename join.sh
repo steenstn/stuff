bba3684af37bedb1dea95d15c34781694e359e23
Alexander Steen <alexander.steen@qapital.com>
2024-02-16 09:22:50 +0100

    Fix regex

--- join.sh
+++ join.sh
@@ -3,12 +3,15 @@
 # Searches for a Google/Zoom link and opens it
 
 meeting_title=$(icalBuddy -n -li 1 -b "" -iep title eventsToday)
-meeting_link=$(icalBuddy -n -li 1 eventsToday | grep -o "https://meet.google.com/\w.*-\w.*-\w." | head -1)
+meeting_link=$(icalBuddy -n -li 1 eventsToday | grep -o "https://meet.google.com/\w.*-\w.*-\w.*" | head -1)
 
 if [[ -z "${meeting_link}" ]]; then
   meeting_link=$(icalBuddy -n -li 1 eventsToday | grep -o "https://wedbush.zoom.us/[^\s]/\d*" | head -1)
 fi
 
-echo "Joining ${meeting_title} at ${meeting_link}"
-
-open ${meeting_link}
+if [[ -z "${meeting_link}" ]]; then
+  echo "No meeting link found to join"
+else
+  echo "Joining ${meeting_title} at ${meeting_link}"
+  open ${meeting_link}
+fi
