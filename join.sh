18dd9f1c96c3ab92aabd64c60c2245d4e9f5aba6
Alexander Steen <alexander.steen@qapital.com>
2024-02-22 13:06:32 +0100

    Exclude all-day events

--- join.sh
+++ join.sh
@@ -2,8 +2,8 @@
 # Uses ical-buddy to find the next upcoming event in the calendar and opens the meeting link contained in the description
 # Searches for a Google/Zoom link and opens it
 
-meeting_title=$(icalBuddy -n -li 1 -b "" -iep title eventsToday)
-meeting_link=$(icalBuddy -n -li 1 eventsToday | grep -o "https://meet.google.com/\w.*-\w.*-\w.*" | head -1)
+meeting_title=$(icalBuddy -n -li 1 -b "" -ea -iep title eventsToday)
+meeting_link=$(icalBuddy -n -li -ea 1 eventsToday | grep -o "https://meet.google.com/\w.*-\w.*-\w.*" | head -1)
 
 if [[ -z "${meeting_link}" ]]; then
   meeting_link=$(icalBuddy -n -li 1 eventsToday | grep -o "https://wedbush.zoom.us/[^\s]/\d*" | head -1)
