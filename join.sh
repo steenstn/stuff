5a6c7a8e3d6a6ef9065e74922ce6c9df53114b18
Alexander Steen <alexander.steen@mynt.se>
2025-03-25 14:36:20 +0100

    Remove zoom link

--- join.sh
+++ join.sh
@@ -6,10 +6,6 @@ meeting_title=$(icalBuddy -n -li 1 -b "" -ea -iep title eventsToday)
 meeting_link=$(icalBuddy -n -li 1 -ea eventsToday | grep -o "https://meet.google.com/\w.*-\w.*-\w.*" | head -1)
 
 if [[ -z "${meeting_link}" ]]; then
-  meeting_link=$(icalBuddy -n -li 1 -ea eventsToday | grep -o "https://wedbush.zoom.us/[^\s]/\d*" | head -1)
-fi
-
-if [[ -z "${meeting_link}" ]]; then
   echo "No meeting link found to join"
 else
   echo "Joining ${meeting_title} at ${meeting_link}"
